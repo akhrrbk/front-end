b0bf9bba02c9457fafc4607a91a3e3e5a96d0022
Akhrorbek Abdukhamitov <akhrrbk@gmail.com>
2022-01-03 00:01:03 +0500

    axios

--- jan2/other/apis.js
+++ jan2/other/apis.js
@@ -16,18 +16,81 @@
 // req.send();
 
 
+
+
+
+
+
+
+
+
+
+
+
 const parag = document.querySelector('p');
-// Fetch API
-fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-    .then(res => {
-        console.log("response, waiting~", res)
-        return res.json()
-    })
-    .then(data => {
-        console.log('data parsed')
-        console.log(data.ticker.price)
-        parag.innerHTML = data.ticker.price;
-    })
-    .catch(e => {
-        console.log('oh, no! error!', e)
-    })
+// // Fetch API
+// fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+//         console.log("response, waiting~", res)
+//         return res.json()
+//     })
+//     .then(data => {
+//         console.log('data parsed')
+//         console.log(data.ticker.price)
+//         parag.innerHTML = +data.ticker.price;
+//     })
+//     .catch(e => {
+//         console.log('oh, no! error!', e)
+//     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// an easy way of fetching with Async await
+
+// const fetchBtcPrice = async () => {
+//     try{
+//         const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
+//         const data = await res.json();
+//         console.log(data.ticker.price)
+//     } catch (e) {
+//         console.log("something went wrong!", e);
+//     }
+// }
+
+
+
+
+
+
+// Axios - promise based
+
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+//         console.log('success');
+//         parag.innerHTML += res.data.ticker.price
+//     })
+//     .catch(e => {
+//         console.log('error!', e)
+//     })
+
+// now same thing with async
+const getBitcoinPrice = async () => {
+    try {
+        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd');
+        // console.log(res.data.ticker.price)
+        parag.innerHTML += res.data.ticker.price
+    } catch (e) {
+        console.log("error!", e)
+    }
+}
+getBitcoinPrice()
